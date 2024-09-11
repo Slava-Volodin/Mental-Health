898f47d6037521c0f742cf29b6e0b6ebbb1b27c0
Yuliia <taragorgodze@gmail.com>
2024-09-11 04:51:22 +0300

    updated header and hero

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from '../../images/logo.png';
+import { useBreakpoints } from '../../utils/hooks/useBreakpoints';
 import s from './index.module.css';
 
 export default function Header() {
+  const { isMobile } = useBreakpoints();
   return (
-    <div className="container">
-      <img alt="logo_icon" src={logo} className={s.logo} />
-    </div>
+    <>
+      {!isMobile && (
+        <div className="container">
+          <img alt="logo_icon" src={logo} className={s.logo} />
+        </div>
+      )}
+    </>
   );
 }
