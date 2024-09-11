2e3cf82b9b89cf03bedcda20f6345eca87ba713a
Slava <kaspermay1997@gmail.com>
2024-09-11 13:24:56 +0300

    add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,17 +6,21 @@ import s from './index.module.css';
 import Links from './Links';
 import Action from '../Action';
 
+import Mental from '../../images/Mental.png';
+
 export default function Footer() {
   return (
     <div className={s.footerSectionWrapper}>
       <div className={classNames('container', s.footerSection)}>
-        <div>
-          <Links />
+        <div className={s.footerTitle}>
+          <img src={Mental}></img>
         </div>
+        <Links />
         <div>
           <Action theme="dark" />
         </div>
-        <div>
+        <div className={s.footerBottom}>
+          <span className={s.footerSpan}>ГО "Ментальні зміни суспільства"</span>
           <Links />
         </div>
       </div>
