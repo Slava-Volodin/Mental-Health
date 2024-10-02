68eeaeecde220f3c0685132da100a26e2f5321db
Slava <kaspermay1997@gmail.com>
2024-10-02 13:40:49 +0300

    update sliderBtn

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
 import s from './index.module.css';
@@ -37,6 +43,10 @@ export default function Carousel() {
             </Slide>
           ))}
         </Slider>
+        <div className={s.sliderBtn}>
+          <ButtonBack>Попередній</ButtonBack>
+          <ButtonNext>наступний</ButtonNext>
+        </div>
       </CarouselProvider>
     </div>
   );
